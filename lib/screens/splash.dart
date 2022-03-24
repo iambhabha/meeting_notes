f146adfc0296e30795c8d34061f8bfa18c0e5b71
iambhabha <bhabha242242@gmail.com>
2022-03-24 23:08:45 +0530

    change

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -13,19 +13,23 @@ class _splashgifState extends State<splashgif> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedSplashScreen(
-        splash: Image.asset('assets/Preloader IV.gif'),
-        nextScreen: NotesPage(),
-        splashTransition: SplashTransition.sizeTransition,
-        splashIconSize: 100,
-        backgroundColor: Colors.black,
-        duration: 7000,
-        // seconds: 8,
-        // backgroundColor: Colors.black,
-        // image: Image.asset('assets/Preloader IV.gif'),
-        // loaderColor: Colors.white,
-        // loadingText: const Text("Welcome To Meeting Notes"),
-        // navigateAfterSeconds: NotesPage(),
+      body: Container(
+        height: 300,
+        width: 400,
+        child: AnimatedSplashScreen(
+          splash: Image.asset('assets/Preloader IV.gif'),
+          nextScreen: NotesPage(),
+          splashTransition: SplashTransition.sizeTransition,
+          splashIconSize: 100,
+          backgroundColor: Colors.black,
+          duration: 5000,
+          // seconds: 8,
+          // backgroundColor: Colors.black,
+          // image: Image.asset('assets/Preloader IV.gif'),
+          // loaderColor: Colors.white,
+          // loadingText: const Text("Welcome To Meeting Notes"),
+          // navigateAfterSeconds: NotesPage(),
+        ),
       ),
     );
   }
