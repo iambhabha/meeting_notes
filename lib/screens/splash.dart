6bf515cd411d0812cf7dd485359c66f9045b2694
iambhabha <bhabha242242@gmail.com>
2022-03-25 13:33:55 +0530

    update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -13,24 +13,48 @@ class _splashgifState extends State<splashgif> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 300,
-        width: 400,
-        child: AnimatedSplashScreen(
-          splash: Image.asset('assets/Preloader IV.gif'),
-          nextScreen: NotesPage(),
-          splashTransition: SplashTransition.sizeTransition,
-          splashIconSize: 100,
-          backgroundColor: Colors.black,
-          duration: 5000,
-          // seconds: 8,
-          // backgroundColor: Colors.black,
-          // image: Image.asset('assets/Preloader IV.gif'),
-          // loaderColor: Colors.white,
-          // loadingText: const Text("Welcome To Meeting Notes"),
-          // navigateAfterSeconds: NotesPage(),
-        ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: 400,
+            child: AnimatedSplashScreen(
+              splash: Image.asset('assets/Preloader IV.gif'),
+              nextScreen: NotesPage(),
+              splashTransition: SplashTransition.sizeTransition,
+              splashIconSize: 100,
+              backgroundColor: Colors.black,
+              duration: 5000,
+
+              // seconds: 8,
+              // backgroundColor: Colors.black,
+              // image: Image.asset('assets/Preloader IV.gif'),
+              // loaderColor: Colors.white,
+              // loadingText: const Text("Welcome To Meeting Notes"),
+              // navigateAfterSeconds: NotesPage(),
+            ),
+          ),
+          text(),
+        ],
       ),
     );
   }
 }
+
+Widget text() => Container(
+      height: 300,
+      width: 400,
+      child: AnimatedSplashScreen(
+        splash: Text("BHABHA\n  \tTechnology",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            )),
+        nextScreen: NotesPage(),
+        splashTransition: SplashTransition.fadeTransition,
+        splashIconSize: 100,
+        backgroundColor: Colors.black,
+        duration: 6000,
+      ),
+    );
